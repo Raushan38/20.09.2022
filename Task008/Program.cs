93080682265db3c2ab91036ed9f71579d56703f1
Raushan <dzhenalieva@gmail.com>
2022-09-21 20:36:23 +0600

    add Task008

---
+++ Task008/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+for (int i = 1; i <= number; i = i + 1) // объявили массив
+{
+    if (i % 2 == 0) // проверка на четность
+    {
+         Console.Write(i + " ");
+    }
+}
