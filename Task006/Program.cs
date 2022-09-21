2bc297df1ccbe9d9d89e5af68a71dd97ccdbcf5b
Raushan <dzhenalieva@gmail.com>
2022-09-21 20:28:04 +0600

    add Task006

---
+++ Task006/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number % 2 == 0) // % - находит дробное значение числа после деления
+{ 
+    Console.WriteLine("четное число");   
+}
+else 
+{
+    Console.WriteLine("нечетное число");  
+}
