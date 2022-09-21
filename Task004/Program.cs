76c024a221b5d3557a895fc7c01629dd6bdf104c
Raushan <dzhenalieva@gmail.com>
2022-09-21 20:27:44 +0600

    add Task004

---
+++ Task004/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("введите первое число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("введите второе число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("введите третье число: ");
+int number3 = Convert.ToInt32(Console.ReadLine());
+
+int max = number1;
+
+if (number1 > max) max = number1;
+if (number2 > max) max = number2;
+if (number3 > max) max = number3;
+
+Console.Write("max = ");
+Console.WriteLine(max);
