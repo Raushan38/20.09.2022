467aa01f882994d78a3b543095301f693b8c25a4
Raushan <dzhenalieva@gmail.com>
2022-09-27 07:05:44 +0600

    add Task_exam

---
+++ Task_exam/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+int num = 13;
+int count = 1;
+while (count <= num)
+{
+    if (count % 2 == 0)
+    {
+        Console.Write($"{count} ");
+    }
+    count++;
+
+}
