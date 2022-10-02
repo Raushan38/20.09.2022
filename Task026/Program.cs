76a1c3dc487108c644e754a17577e7b5139c2b72
Raushan <dzhenalieva@gmail.com>
2022-10-02 10:35:11 +0600

    добавила Task026

---
+++ Task026/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 26. напишите программу которая принимает 
+// на вход число и  выдает количество цифр в числе
+// 456 -> 3
+// 78 -> 2
+// 89123 -> 5
+
+Console.WriteLine("введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int QuantityNumbers(int num)
+{
+    int counter = 0;
+    while (num > 0)
+    {
+        num = num / 10;
+        counter++;
+    }
+    return counter;
+}
+int res = QuantityNumbers(Math.Abs(number));
+Console.WriteLine(res);
