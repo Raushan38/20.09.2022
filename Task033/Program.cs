5d4fb47e2844b92270978513e917c3d77ce87b7f
Raushan <dzhenalieva@gmail.com>
2022-10-05 22:39:52 +0600

    добавила Task033

---
+++ Task033/Program.cs
@@ -0,0 +1,51 @@
+﻿// задача 33. задайте массив. напишите программу которая
+// опредляет присутвует ли заданное число в массиве
+// 4; массив [6,7,19,345,3] -> нет
+// 3; массив [6,7,19,345,3] -> да
+
+System.Console.WriteLine("введите число ");
+int number = Convert.ToInt32(System.Console.ReadLine());
+
+int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] array = new int[size];
+    var rnd = new Random();
+
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = rnd.Next(min, max + 1);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1) Console.Write($"{array[i]}, ");
+        else Console.Write($"{array[i]}");
+    }
+    Console.Write("]");
+}
+
+bool CheckNumber(int[] array, int arg)
+{
+    bool checkNum = false;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(array[i] == arg)
+        {
+            checkNum = true;
+            break;
+        }
+    }
+    return checkNum;
+}
+
+int[] arr = CreateArrayRndInt(12, -9, 9);
+PrintArray(arr);
+bool checkNumber = CheckNumber(arr, number);
+
+if (checkNumber) System.Console.WriteLine("Да");
+else System.Console.WriteLine("Нет");
