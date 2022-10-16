d3666f9e0a7f25afc7b4b3cebd574bf0a03e95a7
Raushan <dzhenalieva@gmail.com>
2022-10-16 15:24:21 +0600

    добавила Task049

---
+++ Task049/Program.cs
@@ -0,0 +1,62 @@
+﻿// задача 49: задайте двумерный массив. найдите
+// элементы у которых индекса четные и замените 
+// элементы на их квадраты
+
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+
+// 1 4 49 2
+// 5 81 2 9
+// 64 4 4 4
+
+int[,] array2D = CreateMatrixRndInt(3, 4, 1, 10);
+PrintMatrix(array2D);
+System.Console.WriteLine();
+GetSquareEvenElem(array2D);
+PrintMatrix(array2D);
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    var rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;  
+}
+
+void GetSquareEvenElem(int[,] matrix)
+
+{
+     for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (i % 2 == 0 && j % 2 == 0) matrix[i, j] *= matrix[i, j];
+        }
+    }
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j], 4} ");
+            else Console.Write($"{matrix[i, j], 4}");
+        }
+        Console.WriteLine("|");
+    }
+}
+
+
+
+
