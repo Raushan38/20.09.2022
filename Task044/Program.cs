afdc2a4d278e54ae04dfa4c89fa9ee39f6c3c4ad
Raushan <dzhenalieva@gmail.com>
2022-10-16 09:03:43 +0600

    добавила Task044

---
+++ Task044/Program.cs
@@ -0,0 +1,25 @@
+﻿// задача 44: не использую рекрусию введите 
+// N числел фибоначчи. первые два числа Ф: 0 и 1
+
+// если N = 5 -> 01123
+// если N = 3 -> 011
+// если N = 7 -> 0112358
+
+
+Console.WriteLine("введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+PrintFibonacci(number);
+
+void PrintFibonacci(int num)
+{
+    int firstNum = 0;
+    int secondNum = 1;
+    Console.Write($"{firstNum} {secondNum} ");
+    for (int i = 2; i < num; i++)
+    {
+        int nextNum = firstNum + secondNum;
+        Console.WriteLine($"{nextNum} ");
+        firstNum = secondNum;
+        secondNum = nextNum;
+    }
+}
