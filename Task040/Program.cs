073a724309ea3db3ad7d46b21f186251f30e7d67
Raushan <dzhenalieva@gmail.com>
2022-10-16 09:02:27 +0600

    добавила Task040

---
+++ Task040/Program.cs
@@ -0,0 +1,15 @@
+﻿// задача 40: напишите программу которая принмает на вход
+// три числа и проверяет может ли существовать треугольник
+// с сторонами дакой длины
+
+Console.WriteLine("введите стороны треугольника");
+int sideA = Convert.ToInt32(Console.ReadLine());
+int sideB = Convert.ToInt32(Console.ReadLine());
+int sideC = Convert.ToInt32(Console.ReadLine());
+if (CanTriangleExite(sideA, sideB, sideC)) Console.WriteLine($"да существует");
+else Console.WriteLine($"нет не существует");
+
+bool CanTriangleExite (int side1, int side2, int side3)
+{
+    return side1 + side2 > side3 && side2 + side3 > side1 && side1 + side3 > side2;
+}
