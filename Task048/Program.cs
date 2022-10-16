4eb2bb9c7ce507d8937987c30327db3378fc23c7
Raushan <dzhenalieva@gmail.com>
2022-10-16 15:24:11 +0600

    добавила Task048

---
+++ Task048/Program.cs
@@ -0,0 +1,39 @@
+﻿// задача 48: задайте двумерный массив рамера  m×n
+// каждый элемент в массива находится
+// по формуле Аmn = m+n Выыедите полученный массив на экран
+
+ // m=3 n=4
+ // 0 1 2 3
+ // 1 2 3 4
+ // 2 3 4 5
+
+ int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+ 
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = i + j;
+        }
+    }
+    return matrix;  
+}
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j], 4} ");
+            else Console.Write($"{matrix[i, j], 4}");
+        }
+        Console.WriteLine("|");
+    }
+}
+int[,] array2D = CreateMatrixRndInt(3, 4, -100, 100);
+PrintMatrix(array2D);
+
+
